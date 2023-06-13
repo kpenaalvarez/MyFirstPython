1695e8801b52cca2696ce90b6dd0826ce3462fc6
kpenaalvarez <136401504+kpenaalvarez@users.noreply.github.com>
2023-06-12 18:24:55 -0700

    Add files via upload

---
+++ sum_Of_First_100.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 17:39:07 2023
+
+@author: kpenaalvarez
+"""
+
+sum100=0
+
+for i in range(100):
+    sum100 = sum100 + (i+1)
+
+print(f'sum of first 100 integers is {sum100}')
